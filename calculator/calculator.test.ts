0a18b4e8731f5b3d08bb2422de63c8a54d3912c1
sobebarali <sobebar_ali17@hotmail.com>
2024-11-28 19:21:58 +0530

    Step 2: Handle single number input

--- calculator/calculator.test.ts
+++ calculator/calculator.test.ts
@@ -5,4 +5,7 @@ describe("calculator", () => {
   test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+  test("returns the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
 });
