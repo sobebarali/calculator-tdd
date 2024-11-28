1bd66c33ebdb7e2be041c76a00c11e3e82b1d397
sobebarali <sobebar_ali17@hotmail.com>
2024-11-28 19:24:09 +0530

    Step 3: Handle two numbers

--- calculator/add.ts
+++ calculator/add.ts
@@ -8,5 +8,6 @@ export function add(numbers: string): number {
   if (numbers === "") {
     return 0;
   }
-  return parseInt(numbers, 10);
+  const numArray = numbers.split(",").map((num) => parseInt(num, 10));
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
--- calculator/calculator.test.ts
+++ calculator/calculator.test.ts
@@ -8,4 +8,7 @@ describe("calculator", () => {
   test("returns the number itself for a single number", () => {
     expect(add("1")).toBe(1);
   });
+  test("returns the sum of two numbers", () => {
+    expect(add("1,2")).toBe(3);
+  });
 });
